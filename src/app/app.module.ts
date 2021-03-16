a28d5ce1c49b6e8dabb903fdaa752371cb91d6db
bkolodii <bkolodiy@binariks.com>
2021-03-16 18:15:15 +0200

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { HeaderItemsComponent } from './component/header/header-items/header-items.component';
+import { MenuComponent } from './component/menu/menu.component';
+import { MenuItemComponent } from './component/menu/menu-item/menu-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeaderItemsComponent,
+    MenuComponent,
+    MenuItemComponent
   ],
   imports: [
     BrowserModule,
