d166b6ae05d30ce6502f5ae2236a3aa08c5e05ee
ystolyarchuk <ystolyarchuk@binariks.com>
2021-06-02 17:50:53 +0300

    add completed compomemt

--- src/app/pages/interviews/interviews-item/completed/completed.component.ts
+++ src/app/pages/interviews/interviews-item/completed/completed.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { map, take } from 'rxjs/operators';
+import { DayEventsService } from 'src/app/service/day-events.service';
+import { RequiredSkillsService } from 'src/app/service/required-skills.service';
+import { Events } from 'src/app/shared/interfaces/events.interface';
+import { RequiredSkills } from 'src/app/shared/interfaces/requiredSkill.interface';
 
 @Component({
   selector: 'app-completed',
@@ -6,10 +12,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./completed.component.scss']
 })
 export class CompletedComponent implements OnInit {
-
-  constructor() { }
+  events: Array<any> = [];
+  constructor(private eventsService: DayEventsService, private router: Router) { }
 
   ngOnInit(): void {
+    this.getRequiredSkills();
   }
 
+  getRequiredSkills(): void {
+    this.eventsService.getAllEvents().snapshotChanges().pipe(
+      map(changes =>
+        changes.map(c =>
+          ({ id: c.payload.doc.id, ...c.payload.doc.data() })
+        )
+      ),
+      take(1)
+      
+    ).subscribe(data => {
+      this.events = data[1].countEvent;
+      // this.getCalendarDays(this.date);
+    });
+
+  }
 }
